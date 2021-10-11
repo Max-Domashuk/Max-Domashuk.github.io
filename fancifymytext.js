5522a6570e24437d4d2b1842d1a65277f4dc0fc2
Max-Domashuk <63775087+Max-Domashuk@users.noreply.github.com>
2021-10-10 23:49:02 -0700

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,42 @@
+
+function testAlert(){
+alert("Hello, world!");
+}
+
+function fancyAlert(){
+    alert("You just made the text Fancy!");
+    }
+
+function lostFancyAlert(){
+    alert("You just removed the fancy text!");
+    }
+
+function bigger(){
+    testAlert();
+    var text = document.getElementById("text");
+    text.style.fontSize="24pt";
+}
+
+function getFancy(){
+    fancyAlert();
+    var text = document.getElementById("text");
+    text.style.fontWeight="bold";
+    text.style.color="blue";
+    text.style.textDecoration="underline";
+}
+
+function removeFancy(){
+    lostFancyAlert();
+    var text = document.getElementById("text");
+    text.style.fontWeight="normal";
+    text.style.color="black";
+    text.style.textDecoration="none";
+}
+
+function moo() {
+    var text = document.getElementById("text");
+    var suffix = "-Moo";
+    text.style.textTransform="uppercase";
+    text.value = text.value.split('.').join('') +suffix;
+
+}
